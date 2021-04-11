b6a20c493ff47a3a8261d766673a3f72c77276ba
Paul Ross <49892782+pross326@users.noreply.github.com>
2021-04-10 23:01:32 -0400

    Create main.js
    Copy and paste code I created in replit

---
+++ main.js
@@ -0,0 +1,50 @@
+//Create random number generator/10
+const random10 = () => {
+  return Math.floor(Math.random()*10)
+}
+//Create random number generator/26
+const random26 = () => {
+  return Math.floor(Math.random()*26)
+}
+
+//List of lowercase
+const lowercaseLetters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+//List of uppercase
+const uppercaseLetters = []
+lowercaseLetters.forEach(letter => uppercaseLetters.push(letter.toUpperCase()))
+
+//List of animals length 10
+const animals = ['lion','tiger','bear','elephant','coyote','parrot','iguana','tern','snake','gecko']
+
+//List of places length 10
+const cities = ['newyork','toronto','chicago','bronx','brooklyn','rolesville','raleigh','ronkonkoma','manhattan','newrochelle']
+
+//Shuffle function
+function shuffle (array) {
+array.sort(() => Math.random() - 0.5)
+}
+
+const createPword = (length=2) => {
+  const Pword = []
+for (let i = 0; i<=length-1; i++){
+  Pword.push(random10())
+}
+for (let i = 0; i<=length-1; i++){
+  Pword.push(lowercaseLetters[random26()])
+}
+for (let i = 0; i<=length-1; i++){
+  Pword.push(uppercaseLetters[random26()])
+}
+for (let i = 0; i<=length-1; i++){
+  Pword.push(animals[random10()])
+}
+for (let i = 0; i<=length-1; i++){
+  Pword.push(lowercaseLetters[random10()])
+}
+shuffle(Pword)
+const password = Pword.join('')
+console.log(password)
+
+}
+createPword()
